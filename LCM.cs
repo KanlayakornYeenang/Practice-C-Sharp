e9c8d9b3ee0a49e69dbd73cd495bcc3537046878
ilovemhuukrob <kanlayakorn.yeenang@gmail.com>
2021-03-28 16:55:57 +0700

    exam

---
+++ LCM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LCM
+{
+    class LCM
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input number a : ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input number b : ");
+            int b = int.Parse(Console.ReadLine());
+
+            int x = a;
+            int y = b;
+
+            do
+            {
+                if (x < y)
+                {
+                    x = x + a;
+                }
+                else
+                {
+                    y = y + b;
+                }
+            } while (x != y);
+
+            Console.WriteLine(x);
+        }
+    }
+}
