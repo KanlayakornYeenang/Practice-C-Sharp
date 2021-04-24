81a659cbfb11777f73ecb6b0a53530411728d7fc
ilovemhuukrob <kanlayakorn.yeenang@gmail.com>
2021-04-24 16:07:31 +0700

    mid term

---
+++ Heaven_stair.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Heaven_stair
+{
+    class Heaven_stair
+    {
+        static void Main(string[] args)
+        {
+            string pp;
+            string s = Console.ReadLine();
+            int w = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int e = 0; e < h; e++)
+                {
+                    pp = " ";
+                    for (int j = 0; j < w; j++)
+                    {
+                        pp += s;
+                    }
+                    //pp = pp * (i + 1);
+                    Console.WriteLine(pp);
+                }
+            }
+        }
+    }
+}
